8312aa7873d3742d612d6ff1c114963a7d86d1ec
abhibobhate <95496264+abhithor@users.noreply.github.com>
2023-05-13 16:41:48 +0530

    second

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -28,7 +28,7 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
         calendar.sort(Comparator.comparing(Meeting::getEndTime));
-        int count=0;
+        int count=1;
         LocalTime lastEndTime=calendar.get(0).getEndTime();
 
         for(int i=1;i<calendar.size();i++){
